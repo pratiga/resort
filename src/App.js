ef6ccd1db9c2670f718700698735e1d1cbf9c8f7
pratiga <gaitrisanu2468@gmail.com>
2022-12-20 20:39:58 +0545

    route
    Signed-off-by: pratiga <gaitrisanu2468@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from './pages/Home';
+import Rooms from './pages/Room';
+import Error from './pages/Error';
+import SingleRoom from './pages/SingleRoom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <BrowserRouter>
+   <Navbar />
+   <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/rooms/" element={<Rooms />} />
+      <Route path="/rooms/:slug" element={<SingleRoom />} />
+      <Route element={<Error />}/>
+   </Routes>
+   </BrowserRouter>
   );
 }
 
